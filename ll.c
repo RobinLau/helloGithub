a8fb8858cda75457523d01c423acc80d33a85c09
Robin Lau <liuyalong.email@gmail.com>
2014-03-22 07:06:34 +0800

    add ll.c

---
+++ ll.c
@@ -0,0 +1,15 @@
+/***************************************************
+* Author: Robin
+* Mail: liuyalong.email@gmail.com
+* Description:
+***************************************************/
+
+#include <stdio.h>
+
+
+int main(int argc, char** argv, char** envp){
+
+
+    return 0;    
+}
+
