82a096b700b1c1eb0651b1d4d75f182b0c0a0668
Robin Lau <liuyalong.email@gmail.com>
2014-03-22 06:43:25 +0800

    add test.c

---
+++ test.c
@@ -0,0 +1,16 @@
+/***************************************************
+* Author: Robin
+* Mail: liuyalong.email@gmail.com
+* Description:
+***************************************************/
+
+#include <stdio.h>
+
+
+int main(int argc, char** argv, char** envp){
+
+  printf ("Hello! First Github Project!!!\n");
+
+    return 0;    
+}
+
